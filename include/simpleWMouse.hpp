a8a5622a6b7c8ad0eda818f347e2ac2568fb9125
Jean Vitor de Paulo <jeanvitordepaulo@gmail.com>
2017-04-22 18:17:26 -0300

    initial commit

---
+++ include/simpleWMouse.hpp
@@ -0,0 +1,207 @@
+#include "simpleWMouse.hpp"
+#include <Windows.h>
+
+namespace simplewmouse{
+
+Mouse::Mouse(){
+	x=0;
+	y=0;
+}
+
+Mouse::~Mouse(){
+}
+
+void Mouse::setXYposition(const int positionX,const int positionY){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the desired
+	location, based on pixel localization */
+	SetCursorPos(positionX,positionY);
+}
+
+int Mouse::getX(){
+	/* Returns the current X position of the cursor*/
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	return (int)cursorPos.x;
+}
+
+int Mouse::getY(){
+	/* Returns the current Y position of the cursor*/
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	return (int)cursorPos.y;
+}
+
+
+void moveTo(const int x,const int y){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the desired
+	location, based on pixel localization */
+	SetCursorPos(x,y);
+}
+void goToTopRight (){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the top right position
+	based on pixel localization */
+	RECT DesktopRect;
+	HWND hDesktop=::GetDesktopWindow();
+	::GetWindowRect(hDesktop, &DesktopRect);
+	SetCursorPos(DesktopRect.right,0);
+}
+
+void goToTopLeft (){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the top left position
+	based on pixel localization */
+	RECT DesktopRect;
+	HWND hDesktop=::GetDesktopWindow();
+	::GetWindowRect(hDesktop, &DesktopRect);
+	SetCursorPos(0,0);
+}
+
+void goToBottomRight (){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the bottom right position
+	based on pixel localization */
+	RECT DesktopRect;
+	HWND hDesktop=::GetDesktopWindow();
+	::GetWindowRect(hDesktop, &DesktopRect);
+	SetCursorPos(DesktopRect.right,DesktopRect.bottom);
+}
+
+void goToBottonLeft (){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to the bottom left position
+	based on pixel localization */
+	RECT DesktopRect;
+	HWND hDesktop=::GetDesktopWindow();
+	::GetWindowRect(hDesktop, &DesktopRect);
+	SetCursorPos(0,DesktopRect.bottom);
+}
+
+void goToCenter (){
+	/* This funcion uses <Windows.h> function SetCursorPos(x,y) to instantly move the mouse cursor to center of the screen
+	based on pixel localization */
+	RECT DesktopRect;
+	HWND hDesktop=::GetDesktopWindow();
+	::GetWindowRect(hDesktop, &DesktopRect);
+	SetCursorPos(DesktopRect.right/2,DesktopRect.bottom/2);
+}
+
+int currentXPosition(){
+	/* Returns the current X position of the cursor*/
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	float currentXPos = cursorPos.x;
+	return (int)currentXPos;
+}
+int currentYPosition(){
+	/* Returns the current Y position of the cursor*/
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	float currentYPos = cursorPos.y;
+	return (int)currentYPos;
+}
+
+bool CheckLeftMouseButtonStatus()
+{
+	/* Verify if the left mouse button is being pressed */
+   if ((GetKeyState(VK_LBUTTON) & 0x80) != 0){
+      return true;
+   }else return false;
+}
+
+bool CheckRightMouseButtonStatus()
+{
+	/* Verify if the right mouse button is being pressed */
+   if ((GetKeyState(VK_RBUTTON) & 0x80) != 0)
+      return true;
+   else return false;
+}
+
+bool CheckWheeltMouseButtonStatus()
+{
+	/* Verify if the Wheel mouse button is being pressed */
+   if ((GetKeyState(VK_MBUTTON) & 0x80) != 0)
+      return true;
+   else return false;
+}
+
+void LeftClick()
+{
+	/* Simulate the left click action */
+	INPUT In={0};													// Create our input.
+
+	In.type        = INPUT_MOUSE;									// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_LEFTDOWN;							// We are setting left mouse button down.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+
+	ZeroMemory(&In,sizeof(INPUT));									// Fills a block of memory with zeros.
+	In.type        = INPUT_MOUSE;									// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_LEFTUP;								// We are setting left mouse button up.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+}
+
+void RightClick()
+{
+	/* Simulate the right click action */
+	INPUT    In={0};													// Create our input.
+
+	In.type        = INPUT_MOUSE;						// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_RIGHTDOWN;							// We are setting left mouse button down.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+
+	ZeroMemory(&In,sizeof(INPUT));									// Fills a block of memory with zeros.
+	In.type        = INPUT_MOUSE;									// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_RIGHTUP;								// We are setting left mouse button up.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+}
+
+void WheelClick()
+{
+	/* Simulate the Wheelbutton click action */
+	INPUT In={0};													// Create our input.
+
+	In.type        = INPUT_MOUSE;									// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_MIDDLEDOWN;							// We are setting left mouse button down.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+
+	ZeroMemory(&In,sizeof(INPUT));									// Fills a block of memory with zeros.
+	In.type        = INPUT_MOUSE;									// Let input know we are using the mouse.
+	In.mi.dwFlags  = MOUSEEVENTF_MIDDLEUP;								// We are setting left mouse button up.
+	SendInput( 1, &In, sizeof(INPUT) );								// Send the input.
+}
+
+//==================================
+void smoothXMovement(const int x,const int smoothness){
+	/* A smooth vertical movement from the current X position to the new x position */
+	POINT cursorPos;
+	int totalMovementX;
+	GetCursorPos(&cursorPos);
+	float currentXPos = cursorPos.x;
+	float currentYPos = cursorPos.y;
+	totalMovementX=x-currentXPos;
+	 if (totalMovementX>0)
+		 for (size_t i=0;i<=totalMovementX;i++){
+			 Sleep(smoothness);
+			 SetCursorPos(currentXPos++,currentYPos);
+		 }
+	 else for(size_t i=totalMovementX;i!=0;i++){
+		 Sleep(smoothness);
+		 SetCursorPos(currentXPos--,currentYPos);
+	 }
+}
+
+void smoothYMovement(const int y, const int smoothness){
+	/* A smooth horizontal movement from the current Y position to the new y position */
+	POINT cursorPos;
+	int totalMovementY;
+	GetCursorPos(&cursorPos);
+	float currentXPos = cursorPos.x;
+	float currentYPos = cursorPos.y;
+	totalMovementY=y-currentYPos;
+	if (totalMovementY>0)
+		 	for (size_t i=0;i<=totalMovementY;i++){
+		 		_sleep(smoothness);
+		 			 SetCursorPos(currentXPos,currentYPos++);
+		 	}
+	else for (size_t i=totalMovementY;i!=0;i++){
+		_sleep(smoothness);
+		 		 SetCursorPos(currentXPos,currentYPos--);
+		  }
+}
+}
