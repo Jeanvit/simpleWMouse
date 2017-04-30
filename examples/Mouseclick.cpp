9181760dac33a5b4ccdd0890160b28d96d408c97
Jean Vitor de Paulo <jeanvitordepaulo@gmail.com>
2017-04-30 13:56:38 -0300

    Adding example

---
+++ examples/Mouseclick.cpp
@@ -0,0 +1,13 @@
+/*Send the cursor to the center of screen, clicks with the Right Button and show the current position */
+
+#include "include/simpleWMouse.hpp"
+#include <iostream>
+
+using namespace simplewmouse;
+
+int main (int argc, const char** argv){
+
+	goToCenter();
+	RightClick();
+	std::cout<<"Mouse is on X:" <<currentXPosition()<< "Y:"<<currentYPosition()<<std::endl;
+}
